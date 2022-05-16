414a577beae53934bc2b404e260e663ea9ae8977
Maxim Firsov <firsoffmaxim@gmail.com>
2022-05-16 09:32:09 +0500

    base project

---
+++ AdvertisingProject/AdvertisingSdk/TechAdvertising.swift
@@ -0,0 +1,13 @@
+//
+//  TechAdvertising.swift
+//  AdvertisingProject
+//
+//  Created by Maksim on 16.05.2022.
+//
+
+import Foundation
+public class TechAdvertising {
+    static public func test(){
+        print("test")
+    }
+}
---
+++ AdvertisingProject/Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Maksim on 16.05.2022.
+//
+
+import UIKit
+import AdvertisingSdk
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("everything is ok")
+        TechAdvertising.test()
+    }
+
+}
+
